c8da1c3288258cc19f26c5b20a375b2c9e5471aa
Ivan Karpushkin <karpushkin_ioann@mail.ru>
2023-03-30 14:13:50 +0300

    Initial commit

---
+++ src/components/more-ticket/more-ticket.tsx
@@ -0,0 +1,7 @@
+import classes from './more-ticket.module.scss';
+
+const MoreTicket = () => {
+  return <button className={classes['btn-more']}>Показать ещё 5 билетов</button>;
+};
+
+export default MoreTicket;
